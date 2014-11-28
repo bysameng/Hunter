58020cae6f0d6d6868a79a369fe3af009cadc85a
ssameng <sam.eng@icloud.com>
2014-11-27 19:32:52 -0500

    init

---
+++ Assets/Scripts/Input/InputProcessor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputProcessor{
+
+    public static Vector3 GetWorldPosition(Vector3 screenPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 100f, Physics.DefaultRaycastLayers);
+        Vector3 worldPosition = hit.point;
+        return worldPosition;
+    }
+
+
+}
---
+++ Assets/Scripts/Input/MouseInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Input.mousePosition;
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 location = InputProcessor.GetWorldPosition(mousePosition);
+            DebugCube.main.cube.transform.position = location;
+        }
+	}
+}
